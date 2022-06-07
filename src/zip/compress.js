99d039b335d2419a6cc4ed1fdc52929f8e7e6809
ivanlasov <lyashov@nwtn-group.com>
2022-06-07 20:16:09 +0300

    fet: add compress function (zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,13 @@
+import path from 'path';
+import fs from 'fs';
+import { createGzip } from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const dirname = path.resolve();
+
+    const gzip = createGzip();
+    const source = fs.createReadStream(path.join(dirname, 'files', 'fileToCompress.txt'));
+    const destination = fs.createWriteStream(path.join(dirname, 'files', 'archive.gz'));
+
+    source.pipe(gzip).pipe(destination);
 };
